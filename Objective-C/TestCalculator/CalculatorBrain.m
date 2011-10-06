ae7bfd3777e129b6829b68b1503d488501c0bef7
Miles McCrocklin <miles.mccrocklin@gmail.com>
2011-10-05 22:03:49 -0700

    Added iPhone development changes

---
+++ Objective-C/TestCalculator/CalculatorBrain.m
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.m
+//  TestCalculator
+//
+//  Created by Miles McCrocklin on 10/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
